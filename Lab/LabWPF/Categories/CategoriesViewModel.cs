1c277645ed79395d1235e88c8f5ee67489f7339a
Andriienko Yelyzaveta <yelyzaveta.andriienko@ukma.edu.ua>
2021-04-04 10:00:03 +0300

    Added Category views

---
+++ Lab/LabWPF/Categories/CategoriesViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LI.CSharp.Lab.GUI.WPF.Navigation;
+using LI.CSharp.Lab.Models.Categories;
+using LI.CSharp.Lab.Services;
+using Prism.Mvvm;
+using Prism.Commands;
+
+namespace LI.CSharp.Lab.GUI.WPF.Categories
+{
+    public class CategoriesViewModel : BindableBase, INavigatable<MainNavigatableTypes>
+    {
+        private CategoryService _service;
+        private CategoryDetailsViewModel _currentCategory;
+        private Action _showCategories;
+        public ObservableCollection<CategoryDetailsViewModel> Categories { get; set; }
+
+        public CategoryDetailsViewModel CurrentCategory
+        {
+            get
+            {
+                return _currentCategory;
+            }
+            set
+            {
+                _currentCategory = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public CategoriesViewModel(Action showCategories)
+        {
+            _service = new CategoryService();
+            Categories = new ObservableCollection<CategoryDetailsViewModel>();
+            foreach (var category in _service.GetCategories())
+            {
+                Categories.Add(new CategoryDetailsViewModel(category));
+            }
+        }
+
+        public MainNavigatableTypes Type
+        {
+            get
+            {
+                return MainNavigatableTypes.Categories;
+            }
+        }
+
+        public void ClearSensitiveData()
+        {
+
+        }
+
+    }
+}
---
+++ Lab/Services/CategoryService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using LI.CSharp.Lab.Models.Users;
+using LI.CSharp.Lab.Models.Wallets;
+
+namespace LI.CSharp.Lab.Services
+{
+    public class CategoryService
+    {
+        private static User u;
+        private static List<Category> c = new List<Category>()
+        {
+            new Category() {
+                Name = "food",
+                Description = "new category food",
+                Color = "red",
+                Icon = new FileInfo("apple")
+            },
+            new Category() {
+                Name = "food",
+                Description = "new category food",
+                Color = "red",
+                Icon = new FileInfo("apple")
+            },
+            new Category() {
+                Name = "food",
+                Description = "new category food",
+                Color = "red",
+                Icon = new FileInfo("apple")
+            },
+        };
+
+        public List<Category> GetCategories()
+        {
+            u.Categories = c;
+            return c.ToList();
+        }
+    }
+}
